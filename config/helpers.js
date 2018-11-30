6f37997d7384b5dc520f63c3a97056aeab6ca212
mattfro333 <mattfro333@gmail.com>
2018-11-29 18:17:52 -0700

    runs but with a lot of errors

--- config/helpers.js
+++ config/helpers.js
@@ -4,7 +4,7 @@ var _root = path.resolve(__dirname, '..');
 
 function root(args) {
   args = Array.prototype.slice.call(arguments, 0);
-  return path.joinapply(path, [_root].concat(args));
+  return path.join.apply(path, [_root].concat(args));
 
 }
 
