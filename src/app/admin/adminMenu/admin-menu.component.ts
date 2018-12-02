0bd6d081d0be264e0675d37de22e189f5c4809de
mattfro333 <mattfro333@gmail.com>
2018-12-02 01:24:09 -0700

    admin bizzy

---
+++ src/app/admin/adminMenu/admin-menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './admin-menu.component.html',
+  styleUrl: ['./admin-menu.component.css']
+})
+
+export class AdminMenuComponent { }
