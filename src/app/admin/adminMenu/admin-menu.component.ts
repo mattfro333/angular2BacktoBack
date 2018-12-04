0e1762ecdcfd0881c59666331703521ce80be0be
mattfro333 <mattfro333@gmail.com>
2018-12-03 22:29:03 -0700

    typo fix

--- src/app/admin/adminMenu/admin-menu.component.ts
+++ src/app/admin/adminMenu/admin-menu.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './admin-menu.component.html',
-  styleUrl: ['./admin-menu.component.css']
+  styleUrls: ['./admin-menu.component.css']
 })
 
 export class AdminMenuComponent { }
