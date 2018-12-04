c2564f33c9f568745d4d3b24a9c8c166569a5eee
mattfro333 <mattfro333@gmail.com>
2018-12-03 23:30:30 -0700

    error section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './start/app.component';
+import { NavComponent } from './shared/navbar.component';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
     imports: [
         BrowserModule,
