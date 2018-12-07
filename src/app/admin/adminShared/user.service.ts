be41fdc02f4512811191dce6ed256a0f1d6b5314
mattfro333 <mattfro333@gmail.com>
2018-12-06 22:49:48 -0700

    setup firebase

--- src/app/admin/adminShared/user.service.ts
+++ src/app/admin/adminShared/user.service.ts
@@ -5,12 +5,20 @@ import {
   ActivateRouteSnapshot,
   RouterStateSnapshot
 } from '@angular/router';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class UserService implements CanActivate {
     userLoggedIn: boolean = false;
 
-    constructor( private router: Router ) { }
+    constructor( private router: Router ) {
+        firebase.initializeApp({
+          apiKey: "AIzaSyDykS_g8v-MFXAxUbi7DpH1lY_U3tXEUTk",
+          authDomain: "gigabytegames-bb2f7.firebaseapp.com",
+          databaseURL: "https://gigabytesgames-bb2f7.firebaseapp.com",
+          messagingSenderId: "434649479866"
+        })
+    }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
       let url: string = state.url;
@@ -21,5 +29,6 @@ export class UserService implements CanActivate {
         if (this.userLoggedIn) { return true; }
 
         this.router.navigate(['/admin/login']);
+        return false;
     }
 }
