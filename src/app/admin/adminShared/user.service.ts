f05fd10fbbd2ba9189fd1dbda865fdee599551b9
mattfro333 <mattfro333@gmail.com>
2018-12-07 14:19:30 -0700

    finished adding firebase API key is irrelevant because it is a tutorial

--- src/app/admin/adminShared/user.service.ts
+++ src/app/admin/adminShared/user.service.ts
@@ -10,6 +10,8 @@ import * as firebase from 'firebase';
 @Injectable()
 export class UserService implements CanActivate {
     userLoggedIn: boolean = false;
+    loggedInUser: string;
+    authUser: any;
 
     constructor( private router: Router ) {
         firebase.initializeApp({
@@ -31,4 +33,38 @@ export class UserService implements CanActivate {
         this.router.navigate(['/admin/login']);
         return false;
     }
+
+      register(email: string, password: string){
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(function(error) {
+            alert(`${error.message} Please Try Again`);
+          });
+
+        verigyUser() {
+          this.authUser = firebase.auth().currentUser;
+          if (this.authUser) {
+
+              alert(`Welcome ${this.authUser.email}`);
+              this.loggedInUser = this.authUser.email;
+              this.userLoggedIn - true;
+              this.router.navigate(['/admin']);
+            }
+          }
+
+          login(loginEmail: string, loginPassword: string) {
+            firebase.auth().signInWithEmailAndPassword(loginEmail, loginPassword)
+              .catch(function(error) {
+                  alert(`${error.message} Unable to login. Try again!`)
+              })
+          }
+
+          logout() {
+            this.userLoggedIn = false;
+            firebase.auth().signout().then(function() {
+                alert(`Logged Out!`);
+
+            }, function(error) {
+                  alert(`${error.message} Unable to login. Try again!`)
+              })
+          }
 }
