68adb388c359db9423d9597c6fd80679742d0c37
mattfro333 <mattfro333@gmail.com>
2018-12-10 22:16:57 -0700

    blog service

---
+++ src/app/admin/adminShared/blog-admin.services.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+import { Blog } from '../adminShared/blog';
+
+@Injectable()
+
+export class BlogAdminService {
+  createPost(post: Blog){
+    let storageRef = firebase.storage().ref();
+    storageRef.child(`images/${post.imgTitle}`).putString(post.img, 'base64')
+        .then((snapshot) => {
+          let url = snapshot.metadata.downloadURLs[0];
+          let dbRef = firebase.database().ref('blogPosts/');
+          let newPost = dbRef.push();
+        })
+  }
+}
