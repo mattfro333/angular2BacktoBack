daec00939718d72326f676aa8848d75d765de568
mattfro333 <mattfro333@gmail.com>
2018-12-13 21:07:45 -0700

    programming we go

---
+++ src/app/admin/adminShared/blog-admin.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../adminShared/user.service'}
+import { Router } from '@angular/router';
+import * as firebase from 'firebase';
+import { BlogAdminService } from '../adminShared/blog-admin.service';
+import { Blog } from '../adminShared/blog';
+
+@Component({
+  templateUrl: './blog-admin.component.html',
+  styleUrls: ['./bolgadmin.component.css']
+})
+
+export class BlogAdminComponent implements OnInit {
+  theUser: string;
+  menuChoice: string;
+  blogPosts: Blog[];
+}
