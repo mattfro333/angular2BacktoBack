1955a32cef4c6ac6c4223f80f24b2ad7b784e809
mattfro333 <mattfro333@gmail.com>
2018-12-18 22:48:50 -0700

    truncate pipe file created

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -14,6 +14,8 @@ import { BlogAdminService } from './adminShared/blog-admin.service';
 import { BlogAdminComponent } from './blogAdmin/blog-admin.component';
 import { BlogAddComponent } from './blogAdd/blog-add.component';
 
+import { TruncatePipe } from './adminShared/trunc.pipe';
+
 const AdminRoutes: Routes = [
   {
     path:'admin',
